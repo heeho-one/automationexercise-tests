5410dec7997e604913a04c8f8e73afa209397007
heeho-one <arvinyadao@outlook.com>
2025-07-24 11:38:52 +0800

    renamed register spec

--- tests/register-account.spec.ts
+++ tests/register-account.spec.ts
@@ -1,17 +1,15 @@
 import { test } from "../e2e/fixtures/page.fixture";
-import { APITestDataUtils } from "../e2e/utils/api-test-data.utils";
-import { APIUtils } from "../e2e/utils/api.utils";
 import { UITestDataUtils } from "../e2e/utils/ui-test-data.utils";
 
 test.describe("User Registration", () => {
-    test("should register a new user", async ({ page, homePage, navBarPage, registrationPage, accountInformationPage, messagePage }) => {
+    test("user should be able to register a new account", async ({ page, homePage, navBarPage, registrationPage, accountInformationPage, messagePage }) => {
+
+        let userInfo = UITestDataUtils.generateRandomUserAccountInformation();
 
         await page.goto("/");
         await homePage.verifyHomePageIsVisible();
         await navBarPage.navigateToSignupLogin();
         await registrationPage.verifySectionHeaderIsVisible();
-
-        let userInfo = UITestDataUtils.generateRandomUserAccountInformation();
         await registrationPage.signup(userInfo.name, userInfo.email);
 
         await accountInformationPage.verifySectionHeaderIsVisible();
