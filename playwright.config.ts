4befff197af930035346f90603715d179ca1719b
heeho-one <arvinyadao@outlook.com>
2025-07-25 08:48:58 +0800

    added integrations to test reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,32 @@
 import { defineConfig, devices } from '@playwright/test';
+import { 
+  CurrentsFixtures,
+  CurrentsWorkerFixtures,
+  currentsReporter
+} from "@currents/playwright";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+const neetoPlaywrightReporterConfig  = {
+  /* Use a unique CI Build ID here.
+  For more details, visit: https://help.neetoplaydash.com/p/a-e8dd98fa */
+  ciBuildId: crypto.randomUUID(),
+  /* Generate an API key and use it here.
+  For more details, visit: https://help.neetoplaydash.com/p/a-e1c8f8ca */
+  apiKey: process.env.NEETO_PLAYDASH_API_KEY || '',
+  projectKey: process.env.NEETO_PLAYDASH_PROJECT_ID || '',
+};
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
+export default defineConfig<CurrentsFixtures, CurrentsWorkerFixtures>({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -22,7 +37,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ["@bigbinary/neeto-playwright-reporter", neetoPlaywrightReporterConfig ],
+    currentsReporter(),
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,7 +48,9 @@ export default defineConfig({
 
     testIdAttribute: 'data-qa',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    video: 'on',
+    screenshot: 'on'
   },
 
   /* Configure projects for major browsers */
